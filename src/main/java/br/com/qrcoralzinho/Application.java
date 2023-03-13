8731cf5d8c3ba498ba84f16a4dfa05bedb14aece
gandalf <m.cavalcantir@outlook.com>
2023-03-13 14:40:30 -0300

    criando os diretorios

--- src/main/java/br/com/qrcoralzinho/Application.java
+++ src/main/java/br/com/qrcoralzinho/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QrcoralzinhoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QrcoralzinhoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
